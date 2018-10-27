363096b3b4379cf10894eb5565a9af98fbd0573e
AshutoshRanghar <ashutoshranghar1@gmail.com>
2018-10-28 02:02:58 +0530

    Next Changes

--- IT_Versit/src/learning/Ranking2.scala
+++ IT_Versit/src/learning/Ranking2.scala
@@ -15,7 +15,7 @@ def getTopNPricedProducts(productsIterable: Iterable[String], topN: Int): Iterab
   topNPricedProducts
 }
   
-  
+   
   
   
   
--- IT_Versit/src/learning/Sets_Operations.scala
+++ IT_Versit/src/learning/Sets_Operations.scala
@@ -6,6 +6,37 @@ object Sets_Operations {
   {
     var sc=new SparkContext("local[*]","Map_Scala")
     var main_rdd=sc.textFile("../dg_raju/orders_data.txt")
-   println(main_rdd.first()) 
+  //Just checking the mapping of the data
+    var mapping_first= main_rdd.map(x=>x.split(",")(1))
+    
+    var get_year=mapping_first.map(x=>x.split("-")(0))
+    get_year.take(5).foreach(println)
+  
+  //Filter_2013 Auguest and September data and map out the order IDs of the customers
+    var august_customer=main_rdd.filter(x=>x.split(",")(1).contains("2013-08")).map(august_customer=>august_customer.split(",")(2))
+    var september_customer=main_rdd.filter(x=>x.split(",")(1).contains("2013-09")).map(sept_customer=>sept_customer.split(",")(2))
+   august_customer.take(5).foreach(println)
+   september_customer.take(5).foreach(println)
+   
+   //Check the count
+   println("The count of august customer data"+august_customer.count())
+   println("The count of Sept customer data"+september_customer.count())
+   
+   
+   
+  // Find Intersection of the data finding common customers in august 2013 and 2013 September
+  //Intersection is DISTINCTTTTTTTTTTTTTTT common elements of data in a set
+   var int_aug_sep=august_customer.intersection(september_customer)
+   
+   //int_aug_sep.foreach(println)
+    println("The count of Union between Aug and Sep "+int_aug_sep.count())
+ //Find the Union of data finding between customers in Aug and sept
+ //Union is common and uncommon data as well also not distinct
+  
+    var union_aug_sep=august_customer.union(september_customer)
+   println("The count of Union between Aug and Sep "+union_aug_sep.count())
+    
+    
+  
   }
 }
